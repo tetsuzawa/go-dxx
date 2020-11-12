e8b59274e5c21aa856a86ed88212495cae217206
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-11-12 11:33:51 +0900

    Add pink noise generater

---
+++ spatial/cmd/make_pinknoise/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/tetsuzawa/go-soundlib/dxx"
+	"github.com/tetsuzawa/go-soundlib/spatial"
+)
+
+func init() {
+	log.SetFlags(0)
+	flag.Usage = func() {
+		log.Printf("Usage: %s signal_length(sample) out(.DXX)\n", filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Printf("error: %+v\n\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	flag.Parse()
+	if flag.NArg() != 2 {
+		return errors.New("invalid arguments")
+	}
+	args := flag.Args()
+	samples, err := strconv.Atoi(args[0])
+	if err != nil {
+		return err
+	}
+	outPath := args[1]
+
+	const fs = 48000
+	pinkNoise := spatial.PinkNoise(samples, fs)
+	return dxx.WriteToFile(outPath, pinkNoise)
+}
